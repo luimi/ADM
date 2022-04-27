ea09264aac6cae230c8babeebf04f1ef6540617f
Luis Meza <luimi@live.com>
2022-04-27 15:52:03 -0500

    build android app

--- client/ionic/capacitor.config.ts
+++ client/ionic/capacitor.config.ts
@@ -1,10 +1,13 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'sidemenusample',
+  appId: 'com.lui2mi.adm.client',
+  appName: 'ADM',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    cleartext: true
+  }
 };
 
 export default config;
