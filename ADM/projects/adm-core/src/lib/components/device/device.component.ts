b7ed2d4ef1e553836b3f7771f2560b00aceaf58a
Luis Miguel Meza <luis.meza@sapexps.com>
2022-05-14 12:56:16 -0500

    build adm-core 0.0.6

--- ADM/projects/adm-core/src/lib/components/device/device.component.ts
+++ ADM/projects/adm-core/src/lib/components/device/device.component.ts
@@ -1,5 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { WebsocketService } from 'adm-core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Device } from '../../models/device.model';
 
 @Component({
