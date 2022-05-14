7d046e6642f62dba1058efbad22a1ee9d33973ab
Luis Miguel Meza <luis.meza@sapexps.com>
2022-05-14 11:34:51 -0500

    added bluetooth

--- ADM/projects/adm-core/src/lib/components/os/os.component.ts
+++ ADM/projects/adm-core/src/lib/components/os/os.component.ts
@@ -8,18 +8,28 @@ import { DeviceComponent } from '../device/device.component';
 })
 export class OperativeSystemComponent extends DeviceComponent implements OnInit {
 
-  constructor() { 
-      super();
+  constructor() {
+    super();
   }
 
   ngOnInit(): void {
 
   }
 
-  shutdown(){
-    this.send({type:'shutdown'});
+  shutdown() {
+    this.send({ type: 'shutdown' });
   }
-  reboot(){
-    this.send({type:'reboot'});
+  reboot() {
+    this.send({ type: 'reboot' });
+  }
+  setadbkey(adbkey: String) {
+    if (!adbkey || adbkey.trim() == "") return;
+    this.send({ type: 'setadbkey', value1: adbkey });
+  }
+  startBluetooth() {
+    this.send({type:'startbluetooth'});
+  }
+  stopBluetooth() {
+    this.send({type:'stopbluetooth'});
   }
 }
